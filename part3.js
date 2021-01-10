6b8d6742f873eae79284a3c63e98bc9c1dac380a
trytomakeit1 <trytomakeit1@gmail.com>
2021-01-10 14:34:07 +0100

    Fix part3 connection to mongodb

--- part3.js
+++ part3.js
@@ -22,17 +22,61 @@ const dbCall = (arguments) => {
         //insert many
         //collection operationType and discipline
         
-        //TODO if operation types and discipline already exist
-        Promise.all([
-            db.collection(operationTypeCollection).insertMany(uniqueOperationList),
-            db.collection(disciplineCollection).insertMany(disciplineList)
-        ]).then((res1, res2) => {
-            console.log("Operation type and discipline inserted successfully");
-        }).catch(
-            e=> console.log(e))
-        .finally(
-            ()=> {mongoclient.close();});
 
+        let operationTypeIdList =[];
+        for(let element of uniqueOperationList) {
+            operationTypeIdList.push(element._id);
+        }
+
+        let disciplineIdList =[];
+        for(let element of disciplineList) {
+            disciplineIdList.push(element._id);
+        }
+
+        let operationTypesToAdd = [];
+        let disciplinesToAdd = [];
+
+        Promise.all([db.collection(operationTypeCollection).find({_id: {$in: operationTypeIdList}}).toArray((err, res) => {
+            
+            if(res && res.length > 0 ) {
+                for(let el of res) {
+                    if (!operationTypeIdList.includes(el._id))
+                    operationTypesToAdd.push(el);
+
+                }
+            } else {
+                operationTypesToAdd = uniqueOperationList;
+            }
+            if(operationTypesToAdd.length > 0 ) {
+                db.collection(operationTypeCollection).insertMany(operationTypesToAdd).then((res)=>{
+
+                }).catch(e => console.log(e))
+            }
+                            
+        }),db.collection(disciplineCollection).find({_id: {$in: disciplineIdList}}).toArray((err, res) => {
+            
+            if(res && res.length > 0 ) {
+                for(let el of res) {
+                    if (!disciplineIdList.includes(el._id))
+                    disciplinesToAdd.push(el);
+                }
+            } else {
+                disciplinesToAdd = disciplineList;
+            }
+            
+            if(disciplinesToAdd.length > 0) {
+                db.collection(disciplineCollection).insertMany(disciplinesToAdd).then((res) => {
+
+                }).catch(e => console.log(e))
+            }
+                            
+        })
+        ]).then((res1, res2) => {
+
+            console.log("Operation type and discipline inserted successfully");    
+
+        }).catch(e => console.log(e));
+         
     })
 
 }
